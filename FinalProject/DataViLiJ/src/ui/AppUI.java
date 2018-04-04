c22136e79b549c0b3fac876fe3067c6d86df4aa4
weixin <weixin.tan@stonybrook.edu>
2018-04-04 00:44:46 -0400

    Layout of the project

---
+++ FinalProject/DataViLiJ/src/ui/AppUI.java
@@ -0,0 +1,25 @@
+package ui;
+
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import vilij.templates.ApplicationTemplate;
+import vilij.templates.UITemplate;
+
+public class AppUI extends UITemplate {
+    ApplicationTemplate applicationTemplate;
+
+    private Button                              scrnShootButton;
+    private Button                              display;
+    private LineChart<Number,Number>            chart;
+    private TextArea                            textArea;
+    private Label                               InfoText;
+
+
+    public AppUI(Stage primaryStage, ApplicationTemplate applicationTemplate) {
+        super(primaryStage, applicationTemplate);
+        this.applicationTemplate = applicationTemplate;
+    }
+}
--- FinalProject/DataViLiJ/src/ui/DataVisualizer.java
+++ FinalProject/DataViLiJ/src/ui/DataVisualizer.java
@@ -1,4 +1,30 @@
 package ui;
 
-public class DataVisualizer {
+
+import javafx.stage.Stage;
+import vilij.templates.ApplicationTemplate;
+import static vilij.settings.InitializationParams.*;
+
+public final class DataVisualizer extends ApplicationTemplate {
+
+    @Override
+    public void start(Stage primaryStage){
+        dialogsAudit(primaryStage);
+        if(propertyAudit())
+            userInterfaceAudit(primaryStage);
+    }
+
+    @Override
+    protected boolean propertyAudit(){
+        boolean failed =(manager==null
+                ||!(loadProperties(PROPERTIES_XML)&& loadProperties(WORKSPACE_PROPERTIES_XML)));
+        if(failed)
+            errorDialog.show(LOAD_ERROR_TITLE.getParameterName(),PROPERTIES_LOAD_ERROR_MESSAGE.getParameterName());
+        return !failed;
+    }
+
+    @Override
+    protected void userInterfaceAudit(Stage primaryStage) {
+        setUIComponent(new AppUI(primaryStage,this));
+    }
 }
